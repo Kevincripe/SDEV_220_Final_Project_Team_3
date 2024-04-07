2c9617925f0f0f79879763d22ea6a0ef6474826a
Kevin Cripe <kevincripe6@gmail.com>
2024-04-07 17:55:00 -0500

    first sales lead model work

--- sales_leads/models.py
+++ sales_leads/models.py
@@ -1,3 +1,23 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class SalesLeads(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    street_address = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    zipcode = models.CharField(max_length=200)
+    telephone = models.CharField(max_length=200)
+    
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.first_name + self.last_name + self.telephone
+    
