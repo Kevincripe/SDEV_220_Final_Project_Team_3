3d5bf31c614275ff6f3a22c6dc6033cf047a3f5c
Kevin Cripe <kevincripe6@gmail.com>
2024-04-07 16:41:42 -0500

    correction

--- sales_leads/urls.py
+++ sales_leads/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.sales_leads, name='sales_leads'),
+    path("", views.index, name='index'),
 ]
 
--- sales_leads/views.py
+++ sales_leads/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def sales_leads(request):
-    return HttpResponse("Hello sales_leads!")
+def index(request):
+    return HttpResponse("Hello, You are at the sales lead index")
