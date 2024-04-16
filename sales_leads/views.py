ed60bdcfdffd81903c15d6157ef9acf0d3c9bc25
Kevin Cripe <kevincripe6@gmail.com>
2024-04-16 17:49:27 -0500

    sales lead list html template

--- sales_leads/views.py
+++ sales_leads/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, You are at the sales lead index")
+
+def sales_lead_list(request):
+    return render(request, 'sales_leads/sales_lead_list.html', {})
