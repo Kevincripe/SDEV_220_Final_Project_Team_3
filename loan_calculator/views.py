b8477023ebe69d459762b0af7786919fb42a353d
Kevin Cripe <kevincripe6@gmail.com>
2024-04-28 19:52:17 -0500

    all funcs working

--- loan_calculator/views.py
+++ loan_calculator/views.py
@@ -17,11 +17,17 @@ def result(request):
     interest_rate = float(request.GET.get('interest_rate'))
     num_of_months = int(request.GET.get('num_of_months'))
     loan_amount = float(request.GET.get('loan_amount'))
-
+    # Checking if Interest rate field is Zero
     if request.GET.get('interest_rate') == "0":
-        ans = (loan_amount - down_payment) / num_of_months
+        # no interest means simple calculation
+        monthly_payment = (loan_amount - down_payment) / num_of_months 
+        return render(request, 'loan_calculator/result.html', {'monthly_payment': monthly_payment, 'total_paid': loan_amount, 'total_interest': '0'} )
         
-    elif request.GET.get('interest_rate') >= "0":    
-        ans = '0'
-
-    return render(request, 'loan_calculator/result.html', {'ans': ans})
+    # If interest is invovled have to calc monthly interest and compute
+    elif request.GET.get('interest_rate') >= "0": 
+        monthly_interest_rate = interest_rate / 1200  
+        monthly_payment = (loan_amount * monthly_interest_rate ) / (1-(1 + monthly_interest_rate)**(- num_of_months))
+        total_paid = monthly_payment * num_of_months
+        total_interest = total_paid - loan_amount
+        return render(request, 'loan_calculator/result.html', \
+            {'monthly_payment': monthly_payment, 'total_paid': total_paid, 'total_interest': total_interest})
